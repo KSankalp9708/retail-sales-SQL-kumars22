caa84628d05f27302568ac467d450f34055176dd
KSankalp9708 <kumarsankalp9708@gmail.com>
2025-08-29 21:22:21 +0530

    Creating database and table

--- sql/01_schema_mysql.sql
+++ sql/01_schema_mysql.sql
@@ -1,5 +1,4 @@
--- Creating database and main table
-DROP DATABASE IF EXISTS retail_sql_ksankalp;
+-- Creating database
 CREATE DATABASE retail_sql_ksankalp;
 USE retail_sql_ksankalp;
 
@@ -7,17 +6,16 @@ USE retail_sql_ksankalp;
 -- Core sales table
 CREATE TABLE Retail_Sales (
 Transaction_ID INT PRIMARY KEY,
-sale_date DATE NOT NULL,
-sale_time TIME NULL,
-Customer_ID INT NOT NULL,
-gender ENUM('Male','Female','Other') NOT NULL,
-age INT CHECK (age BETWEEN 10 AND 100),
-category VARCHAR(50) NOT NULL,
-quantity INT NOT NULL CHECK (quantity >= 0),
+sale_date DATE,
+sale_time TIME ,
+Customer_ID INT,
+gender ENUM('Male','Female','Other'),
+age INT,
+category VARCHAR(50),
+quantity INT,
 price_per_unit DECIMAL(10,2),
 cogs DECIMAL(10,2),
 total_sale DECIMAL(10,2),
-
 
 -- Derived fields
 sale_timestamp DATETIME GENERATED ALWAYS AS (
@@ -25,8 +23,3 @@ TIMESTAMP(sale_date, COALESCE(sale_time, '00:00:00'))
 ) VIRTUAL,
 margin DECIMAL(10,2) GENERATED ALWAYS AS (total_sale - cogs) VIRTUAL
 ) ENGINE=InnoDB;
-
-
--- Optional reference tables for future normalization
--- CREATE TABLE customers (...);
--- CREATE TABLE products (...);
