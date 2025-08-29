994910b229ae744378ba5054af373a01bd541e08
KSankalp9708 <kumarsankalp9708@gmail.com>
2025-08-29 22:49:09 +0530

    Basic Data Exploration and Analysis

--- sql/04_Basic_Data_Exploration_and_Analysis_mysql.sql
+++ sql/04_Basic_Data_Exploration_and_Analysis_mysql.sql
@@ -7,21 +7,22 @@ SELECT COUNT(*) as total_sale FROM Retail_Sales
 SELECT COUNT(DISTINCT customer_id) as total_sale FROM Retail_Sales
 
 -- How many categories of items that were sold?
-SELECT DISTINCT category FROM retail_sales
+SELECT DISTINCT category FROM Retail_Sales
 
 -----------------------------------------------------------------------------------
 
 --Basic Data Analysis Questions & Answers
+  
 -- 1) SQL query to retrieve all columns for sales made on '2022-11-05:
 SELECT *
-FROM retail_sales
+FROM Retail_Sales
 WHERE sale_date = '2022-11-05';
 
--- 2)SQL query to retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
+-- 2)SQL query to retrieve all transactions where the category is 'Electronics' and the quantity sold is more than 4 in the month of Nov-2022
 SELECT *
-FROM retail_sales
+FROM Retail_Sales
 WHERE 
-    category = 'Clothing'
+    category = 'Electronics'
     AND 
     TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
     AND
@@ -31,20 +32,21 @@ WHERE
 
 SELECT 
     category,
-    SUM(total_sale) as net_sale,
-    COUNT(*) as total_orders
-FROM retail_sales
+    SUM(total_sale) as Net_sale,
+    COUNT(*) as Total_orders
+FROM Retail_Sales
 GROUP BY 1
 
 -- 4) SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 
 SELECT
-    ROUND(AVG(age), 2) as avg_age
-FROM retail_sales
+    ROUND(AVG(age), 2) as avg_beauty_age
+FROM Retail_Sales
 WHERE category = 'Beauty'
 
 
--- 5) SQL query to find all transactions where the total_sale is greater than 1000.
+-- 5) SQL query to find the number of transactions where the total_sale is greater than 1000.
 
-SELECT * FROM retail_sales
+SELECT COUNT(*) as total_high_sales
+FROM Retail_Sales
 WHERE total_sale > 1000
